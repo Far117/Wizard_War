224ab2500d9a62617f10628b38b3bfeecc4f51fd
Far117 <farscience@hotmail.com>
2014-06-25 18:17:37 -0400

    Init

--- main.cpp
+++ main.cpp
@@ -2,8 +2,54 @@
 
 using namespace std;
 
+struct info{
+    float version=0;
+    string type="pre-alpha";
+};
+
+string inputS(string x){
+
+    string temp;
+
+    cout << x;
+    getline(cin,temp);
+    //cout << endl;
+
+    return temp;
+}
+
+int inputI(string x){
+    int temp;
+
+    cout << x;
+    cin >> temp;
+    cout << endl;
+
+    cin.ignore();
+
+    return temp;
+}
+
+float inputF(string x){
+    float temp;
+
+    cout << x;
+    cin >> temp;
+    cout << endl;
+
+    cin.ignore();
+
+    return temp;
+}
+
+void splash(){
+    cout << "          Wizard War" << endl;
+    cout << "     " << type << " v " << version << endl;
+    cout << "        Press enter...." << endl;
+    inputS("");
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
     return 0;
 }
